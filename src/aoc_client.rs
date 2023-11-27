78e4b2f854f297029066f5a0c5b0267953770cc9
Jan Gosmann <jan@hyper-world.de>
2023-11-27 21:17:23 +0100

    Add client to download inputs

---
+++ src/aoc_client.rs
@@ -0,0 +1,63 @@
+use anyhow::{anyhow, Context};
+use bytes::Bytes;
+use futures_core::Stream;
+use reqwest::{
+    self,
+    header::{HeaderMap, HeaderValue},
+    Client, ClientBuilder, Url,
+};
+use secrecy::{ExposeSecret, Secret};
+use tokio_stream::StreamExt;
+
+pub struct AocClient {
+    client: Client,
+    base_url: Url,
+}
+
+impl AocClient {
+    pub fn new(mut base_url: Url, session_id: Secret<String>) -> anyhow::Result<Self> {
+        if base_url.cannot_be_a_base() {
+            return Err(anyhow!("base URL is not a valid base"));
+        }
+        if base_url.path_segments().unwrap().last() != Some("") {
+            base_url.path_segments_mut().unwrap().push("");
+        }
+        let mut headers = HeaderMap::with_capacity(1);
+        let mut session_id =
+            HeaderValue::from_bytes(format!("session={}", session_id.expose_secret()).as_bytes())
+                .map_err(|_| anyhow!("invalid bytes in session ID"))?;
+        session_id.set_sensitive(true);
+        headers.insert("Cookie", session_id);
+        Ok(Self {
+            client: ClientBuilder::new()
+                .default_headers(headers)
+                .build()
+                .expect("couldn't initialize HTTP client"),
+            base_url,
+        })
+    }
+
+    pub async fn get_input(
+        &self,
+        year: i32,
+        day: u32,
+    ) -> anyhow::Result<impl Stream<Item = anyhow::Result<Bytes>>> {
+        // path_segments_mut cannot error because pre-conditions are checked
+        // on instantiation
+        let mut url = self.base_url.clone();
+        url.path_segments_mut().unwrap().extend(&[
+            &year.to_string(),
+            "day",
+            &day.to_string(),
+            "input",
+        ]);
+        Ok(self
+            .client
+            .get(url)
+            .send()
+            .await
+            .context("HTTP GET")?
+            .bytes_stream()
+            .map(|bytes| bytes.context("reading HTTP response")))
+    }
+}
