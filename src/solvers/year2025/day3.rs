46b72ee6934d1ddf587fedfd39fb85ebf964774d
Jan Gosmann <jan@hyper-world.de>
2025-12-03 09:16:45 +0100

    Solve year 2025 day 3 part 2

--- src/solvers/year2025/day3.rs
+++ src/solvers/year2025/day3.rs
@@ -20,30 +20,48 @@ impl<'input> Solver<'input> for SolverImpl<'input> {
     }
 
     fn solve_part_2(&self) -> anyhow::Result<Solution> {
+        let output_voltage: u64 = self
+            .banks
+            .iter()
+            .copied()
+            .map(max_joltage_with_override)
+            .sum();
         Ok(Solution::with_description(
-            "Part 2",
-            "not implemented".to_string(),
+            "Output voltage with override",
+            output_voltage.to_string(),
         ))
     }
 }
 
 fn max_joltage(bank: &[u8]) -> u64 {
-    let (first_idx, first_value) =
-        bank[..bank.len() - 1]
-            .iter()
-            .enumerate()
-            .fold(
-                (0, bank[0]),
-                |acc, (idx, &value)| {
-                    if value > acc.1 {
-                        (idx, value)
-                    } else {
-                        acc
-                    }
-                },
-            );
+    let (first_idx, first_value) = next_max(&bank[..bank.len() - 1]);
     let second_value = bank[first_idx + 1..].iter().max().unwrap_or(&0);
     ((first_value - b'0') * 10 + (second_value - b'0')) as u64
+}
+
+fn max_joltage_with_override(bank: &[u8]) -> u64 {
+    (0..12)
+        .fold((0usize, 0u64), |(start_idx, joltage), i| {
+            let (max_idx, value) = next_max(&bank[start_idx..bank.len() - 11 + i]);
+            (
+                start_idx + max_idx + 1,
+                joltage * 10 + ((value - b'0') as u64),
+            )
+        })
+        .1
+}
+
+fn next_max(bank: &[u8]) -> (usize, u8) {
+    bank.iter().enumerate().fold(
+        (0, bank[0]),
+        |acc, (idx, &value)| {
+            if value > acc.1 {
+                (idx, value)
+            } else {
+                acc
+            }
+        },
+    )
 }
 
 #[cfg(test)]
@@ -61,7 +79,7 @@ mod test {
     #[test]
     fn test_example_part_2() -> anyhow::Result<()> {
         let solver = SolverImpl::new(include_str!("./day3-1.example"))?;
-        assert_eq!(solver.solve_part_2()?.solution, "TODO");
+        assert_eq!(solver.solve_part_2()?.solution, "3121910778619");
         Ok(())
     }
 }
